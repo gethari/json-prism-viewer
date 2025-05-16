3ada168308e1af8005f8dc5a164a01d48a86ca08
Hari Haran <hharan618@gmail.com>
2025-05-16 12:23:55 +0000

    try hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {  HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 import { ThemeProvider } from './hooks/use-theme';
@@ -10,15 +10,14 @@ import JsonCompare from './pages/JsonCompare';
 import TranslationChecker from './pages/TranslationChecker';
 
 const queryClient = new QueryClient();
-// Using process.env.NODE_ENV to determine if in production
-const basename = process.env.NODE_ENV === 'production' ? '/json-prism-viewer' : '';
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme="light" storageKey="json-prism-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={basename}>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/json-compare" element={<JsonCompare />} />
@@ -26,7 +25,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
